e716bf0312061f8b54144ecb3f4ca2f0624db1b2
Esteban Acevedo <tucorreo@correo>
2024-09-07 11:28:23 -0300

    feat: modelos Pais y Cliente

--- project/clientes/models.py
+++ project/clientes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    nacimiento = models.DateField(null=True, blank=True)
+    dni = models.CharField(max_length=15, null=True, blank=True)
+    pais_origen = models.ForeignKey(Pais, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.apellido}, {self.nombre}'
