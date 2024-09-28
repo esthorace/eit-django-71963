69205d116aefc217bc5168b7cd2e210b2fa4b7d1
Esteban Acevedo <tucorreo@correo>
2024-09-28 12:55:47 -0300

    feat: modelo Producto

--- project/productos/admin.py
+++ project/productos/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-from .models import ProductoCategoria
+from .models import Producto, ProductoCategoria
 
 admin.site.register(ProductoCategoria)
+
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('categoria', 'nombre', 'stock', 'unidad_medida', 'precio')
+    list_filter = ('categoria', 'unidad_medida')
+    search_fields = ('nombre', 'categoria__nombre')
--- project/productos/models.py
+++ project/productos/models.py
@@ -11,3 +11,39 @@ class ProductoCategoria(models.Model):
     class Meta:
         verbose_name = 'Categoría de Producto'
         verbose_name_plural = 'Categorías de Productos'
+
+
+class Producto(models.Model):
+    """Productos corresponden a una categoría"""
+
+    categoria = models.ForeignKey(
+        ProductoCategoria,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='categoría',
+    )
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(null=True, blank=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    unidad_medida = models.CharField(max_length=50)
+    stock = models.FloatField()
+    fecha_actualizacion = models.DateTimeField(editable=False, auto_now=True)
+
+    def __str__(self):
+        return f'{self.nombre} ({self.unidad_medida}) ${self.precio:.2f}'
+
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+        ordering = ['-fecha_actualizacion']
+        # La siguiente línea es para que no se repitan los productos
+        # unique_together = ('categoria', 'nombre')
+        constraints = [
+            models.UniqueConstraint(
+                fields=['categoria', 'nombre'],
+                name='unique_producto_categoria_nombre',
+            )
+        ]
+        # Indexación
+        indexes = [models.Index(fields=['nombre'])]
